9b5caf0b54d09e34d299723745a6998183d761d4
Norddin Oubahman <n.oubahman@hotmail.com>
2025-03-31 21:15:14 +0200

    Install reanimated and react query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="detail" />
-      <Stack.Screen name="search" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="detail" />
+        <Stack.Screen name="search" />
+      </Stack>
+    </QueryClientProvider>
   );
 }
